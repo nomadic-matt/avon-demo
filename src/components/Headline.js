4e86336ebe5de94e53f323737f57f4f7b676d021
Matt <matt.rajkovic@magnolia-cms.com>
2023-05-11 10:15:50 +0200

    update

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Headline = props => 
 <div>
     <h2 className="Headline">{props.text}</h2>
-    <h4 className="Headline">{props.subtitle}</h4> 
 </div>
 
 export default Headline;
